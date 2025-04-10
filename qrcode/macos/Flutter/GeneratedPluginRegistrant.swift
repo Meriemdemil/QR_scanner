32f62b1d4159d030c8a886e9baea9848bf49f0a3
Meriemdemil <meriem.demil@ensia.edu.dz>
2025-04-10 20:43:00 +0100

    initial commit (check the readme

---
+++ qrcode/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import mobile_scanner
+import openpgp
+import url_launcher_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+  OpenpgpPlugin.register(with: registry.registrar(forPlugin: "OpenpgpPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+}
